01e3280ee00b20f5e9495e26f4cc36f4ed76c1ac
nerdedi <nerdedi@windgap.org.au>
2026-02-19 11:13:47 +0000

    test(game): add DOM + e2e smoke tests for Adventure page

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,16 +1,24 @@
+/* eslint-env mocha */
+/* global cy, describe, it, beforeEach */
 describe('SmartLearning (E2E)', () => {
   beforeEach(() => {
-    cy.visit('/');
-  });
+    cy.visit('/')
+  })
 
   it('loads the app and performs the main action', () => {
-    cy.get('h1').contains('SmartLearning');
-    cy.get('#actionBtn').should('be.visible').click();
-    cy.get('#result').should('have.text', '2 + 3 = 5');
-  });
+    cy.get('h1').contains('SmartLearning')
+    cy.get('#actionBtn').should('be.visible').click()
+    cy.get('#result').should('have.text', '2 + 3 = 5')
+  })
 
   it('serves the manifest and icons', () => {
-    cy.request('/manifest.webmanifest').its('status').should('eq', 200);
-    cy.request('/icons/icon-192.png').its('status').should('eq', 200);
-  });
-});
+    cy.request('/manifest.webmanifest').its('status').should('eq', 200)
+    cy.request('/icons/icon-192.png').its('status').should('eq', 200)
+  })
+
+  it('opens the Adventure game page', () => {
+    cy.get('#nav-game').should('have.attr', 'href', 'game/index.html')
+    cy.visit('/game/index.html')
+    cy.get('#start-adventure').should('be.visible')
+  })
+})
